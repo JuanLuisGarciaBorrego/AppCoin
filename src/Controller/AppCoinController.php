97321fe2c655629a4c7fe746dc716073414f3c7d
juanluisgarciaborrego <juanluisgarciaborrego@gmail.com>
2018-02-19 20:41:41 +0100

    extends symfony controller

--- src/Controller/AppCoinController.php
+++ src/Controller/AppCoinController.php
@@ -3,12 +3,12 @@
 namespace App\Controller;
 
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class AppCoinController
+class AppCoinController extends Controller
 {
     public function homepage()
     {
-        return new Response('Hello world');
+        return $this->render('homepage.html.twig');
     }
 }
