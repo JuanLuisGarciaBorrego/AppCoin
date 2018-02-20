68929ad90ea94e5435a5ab061c8b6346ba909cec
juanluisgarciaborrego <juanluisgarciaborrego@gmail.com>
2018-02-20 12:58:37 +0100

    routes wallet & save

--- src/Controller/AppCoinController.php
+++ src/Controller/AppCoinController.php
@@ -15,4 +15,20 @@ class AppCoinController extends Controller
     {
         return $this->render('homepage.html.twig');
     }
+
+    /**
+     * @Route("/wallet", name="wallet")
+     */
+    public function wallet()
+    {
+        return $this->render('wallet.html.twig');
+    }
+
+    /**
+     * @Route("/save", name="save")
+     */
+    public function save()
+    {
+        return $this->redirectToRoute('wallet');
+    }
 }
