04560284c25bb13d81c18a825f8ad29ff47f6512
juanluisgarciaborrego <juanluisgarciaborrego@gmail.com>
2018-02-20 12:44:07 +0100

    routas con anotaciones

--- src/Controller/AppCoinController.php
+++ src/Controller/AppCoinController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AppCoinController extends Controller
 {
+    /**
+     * @Route("/", name="homepage")
+     */
     public function homepage()
     {
         return $this->render('homepage.html.twig');
